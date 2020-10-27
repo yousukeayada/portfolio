99866dd36274b61bda58613fea89d21c2ee2ff2b
綾田陽介 <202x102x@stu.kobe-u.ac.jp>
2020-10-28 02:49:47 +0900

    About の内容を追加。

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="mt-5 p-5">
         <h1 class="mb-5 text-center"><img src="../../static/img/about.png">About</h1>
-        
-        初めまして。大学院生のユーと申します。情報系を専攻しており、研究では無線LANや強化学習を取り扱っています。
+        <!-- <img src="../../static/img/myicon.jpg"><br> -->
+        <div class="myicon"></div>
+        <table class="mt-5 table table-hover table-borderless text-center">
+            <tr><td>Name</td><td>ユー</td></tr>
+            <tr><td>Position</td><td>情報系専攻の大学院生</td></tr>
+            <tr><td>Study</td><td>無線LAN、強化学習</td></tr>
+            <tr><td>Like</td><td>アニメ、漫画/THE IDOLM@STER/ONE PIECE</td></tr>
+        </table>
     </div>
 </template>
 
@@ -15,5 +21,22 @@ export default {
 <style scoped>
 div {
     color: white;
+}
+.myicon {
+    background-image: url("../../static/img/myicon.jpg");
+    border-radius: 50%;
+    background-position: 50% 50%;
+    width: 200px;
+    height: 200px;
+    margin: 0 auto;
+}
+table {
+    max-width: 500px;
+    margin: 0 auto;
+    color: white;
+    border-radius: 10px;
+    background: #474848;
+    box-shadow: 5px 5px 10px #3c3d3d, 
+                -5px -5px 10px #525353;
 }
 </style>
