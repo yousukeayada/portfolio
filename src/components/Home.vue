1b5ec9b199860c78ad283ea64f6511d852b0b1dd
綾田陽介 <202x102x@stu.kobe-u.ac.jp>
2020-10-29 16:10:21 +0900

    フォントや背景を設定。

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
-    <div>
   <b-jumbotron header="Welcome" lead="ここはユーのポートフォリオサイトです。">
   </b-jumbotron>
-</div>
   </div>
 </template>
 
@@ -28,10 +26,15 @@ export default {
 
 <style scoped>
 div {
-    color: white;
-    border-radius: 10px;
-    background: #474848;
-    box-shadow:  8px 8px 16px #3c3d3d, 
-                -8px -8px 16px #525353;
+  background: url("/static/img/home.jpg") center no-repeat;
+  background-size: cover;
+  background-color: rgba(255,255,255,0.1);
+  background-blend-mode: lighten;
+  font-family: Times, serif;
+  color: gray;
+  border-radius: 10px;
+  /* background: #474848; */
+  box-shadow:  8px 8px 16px #3c3d3d, 
+              -8px -8px 16px #525353;
 }
 </style>
