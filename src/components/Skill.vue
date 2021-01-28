cf6b063f0db1ec01e00e32fe7a091acca65af28c
綾田陽介 <202x102x@stu.kobe-u.ac.jp>
2021-01-28 19:07:51 +0900

    modify Home and Skill

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -7,7 +7,8 @@
         <Card tech='C++' text='研究や Atcoder で使用しています。' rate='3'></Card>
         <Card tech='Java' text='大学の講義で扱いました。' rate='1.5'></Card>
         <Card tech='Spring Boot' text='大学の講義で扱いました。Web アプリ制作経験があります。' rate='1.5'></Card>
-        <Card tech='Python' text='API 叩いたりちょっとしたデータ分析をするときによく使います。' rate='2'></Card>
+        <Card tech='Python' text='API 叩いたりちょっとしたデータ分析をするときによく使います。' rate='2.5'></Card>
+        <Card tech='Django' text='ハッカソン参加時に使ったことがあります。' rate='1.5'></Card>
         <Card tech='JavaScriptt' text='基礎的なことはわかります。' rate='1.5'></Card>
         <Card tech='Vue.js' text='このポートフォリオを作るのに勉強しました。' rate='1'></Card>
         <Card tech='Node.js' text='開発のために使っていますが、サーバーサイドとしてはサンプルを動かしたことがある程度です。' rate='1'></Card>
@@ -19,15 +20,15 @@
 
     <h2 class="m-5 p-3 text-info text-center">Cloud</h2>
     <div class="skill">
-        <Card tech='Heroku' text='a little' rate='2'></Card>
-        <Card tech='AWS' text='a little' rate='2'></Card>
-        <Card tech='GCP' text='a little' rate='2'></Card>
+        <Card tech='Heroku' text='Bot を常駐させたり、Web サイト公開のために利用しています。' rate='2'></Card>
+        <Card tech='AWS' text='EC2 の構築など基本的な操作経験はあります。' rate='1.5'></Card>
+        <Card tech='GCP' text='VM インスタンスの構築など基本的な操作経験はあります。' rate='1.5'></Card>
     </div>
 
     <h2 class="m-5 p-3 text-center">Tool</h2>
     <div class="skill">
         <Card tech='Git' text='基本的な操作はできます。チームでの使用経験はまだ浅いです。' rate='2'></Card>
-        <Card tech='VS Code' text='a little' rate='2'></Card>
+        <Card tech='VS Code' text='エディタとして普段使っています。' rate='3'></Card>
     </div>
 </div>
 </template>
